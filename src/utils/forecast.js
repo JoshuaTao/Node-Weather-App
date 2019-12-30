bcda26d53b575a1d2e38ac051de6e0c47b0b0e63
Wei Tao <wei@gmail.com>
2019-12-30 17:13:37 +0800

    changed forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,8 +1,8 @@
 const request = require("request");
 
 const forecast = (longitude, latitude, callback) => {
-  const url = `https://api.darksky.net/forecast/3cf4c67e6e24591f42e9657a4797876b/${latitude},${longitude}`;
-  request({ url, json: true }, (error, {body}) => {
+  const url = `https://api.darksky.net/forecast/3cf4c67e6e24591f42e9657a4797876b/${latitude},${longitude}/?units=si`;
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to services!", undefined);
     } else if (body.error) {
@@ -10,7 +10,7 @@ const forecast = (longitude, latitude, callback) => {
     } else {
       callback(
         undefined,
-        `It is currently ${body.currently.temperature} degrees out.There is a ${body.currently.precipProbability}% chance of rain.`
+        `${body.daily.data[0].summary}.The highest/lowest temperature is ${body.daily.data[0].temperatureMax}/${body.daily.data[0].temperatureMin} degrees.It is currently ${body.currently.temperature} degrees out.There is a ${body.currently.precipProbability}% chance of rain.`
       );
     }
   });
